e0e692c02e6b9ca2d331ad979cc73beb6ca1b489
smirithika <smirithichandrasegar@gmail.com>
2018-03-31 14:39:34 +0530

    userview completed

--- ourProject1/src/com/ourProject1/userView/UserViewController.java
+++ ourProject1/src/com/ourProject1/userView/UserViewController.java
@@ -2,9 +2,8 @@ package com.ourProject1.userView;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,86 +11,62 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.ourProject1.encrypt.perform;
-import com.ourProject1.register.RegisterDao;
-
 /**
  * Servlet implementation class UserViewController
  */
 @WebServlet("/UserView")
 public class UserViewController extends HttpServlet {
-	
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html;charset=UTF-8");
 		PrintWriter out=response.getWriter();
 		
-		String fname, lname, pAddress, cAddress, mobNo, homeNo, email,pwd,conPwd;
-		String regex ="^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#\\$%\\^&\\*])(?=.{8,})";
+		String pAddress, cAddress, mobNo, homeNo;
+	    Pattern p = Pattern.compile("^[0-9]{10}$");
+		boolean valid = true;
 		
-		fname=request.getParameter("fname").toString();
-		lname=request.getParameter("lname").toString();
+		String email=request.getParameter("email").toString();
+		System.out.println(email);
+		
 		pAddress=request.getParameter("p_address").toString();
 		cAddress=request.getParameter("c_address").toString();
 		mobNo=request.getParameter("mobile").toString();
 		homeNo=request.getParameter("home_tel").toString();
-		email=request.getParameter("email").toString();
-		pwd=request.getParameter("pwd").toString();
-		conPwd=request.getParameter("con_pwd").toString();
-		
-		SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd");
-		java.util.Date date;
-		java.sql.Date sqlStartDate = null;
-		try {
-			date = sdf1.parse(request.getParameter("DoB").toString());
-			sqlStartDate = new java.sql.Date(date.getTime()); 
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-		//System.out.println(fname+lname+sqlStartDate+pAddress+cAddress+mobNo+homeNo+email+pwd+conPwd+" =output");
 		
 		String select[]= request.getParameterValues("selected");
 		if(select == null) {
 			out.println("<script type=\"text/javascript\">");
 	        out.println("alert('Select atleast one interest.');");
-	        out.println("location='Register.jsp';");
+	        out.println("location='UserView.jsp';");
 	        out.println("</script>");
+			System.out.println("Select atleast one interest");
+	        valid = false;
 		}
-		else if (sqlStartDate.after(new Date())) {
+		Matcher matcher2 = p.matcher(mobNo);
+		if (!matcher2.matches()) {
 			out.println("<script type=\"text/javascript\">");
-	        out.println("alert('Date invalid.\nCannot be a future date.');");
-	        out.println("location='Register.jsp';");
+	        out.println("alert('Invalid Mobile No. Cannot contain characters.');");
+	        out.println("location='UserView.jsp';");
 	        out.println("</script>");
+	        System.out.println("mob inval");
+	        valid = false;
 		}
-		else if (pwd.matches(regex)) {
+		Matcher matcher3 = p.matcher(homeNo);
+		if (!matcher3.matches()) {
 			out.println("<script type=\"text/javascript\">");
-	        out.println("alert('password should contains atleast 8 characters including a digit,a lower case,an uppercase letter,a special character and no spaces allowed');");
-	        out.println("location='Register.jsp';");
+	        out.println("alert('Invalid Home No. Cannot contain characters.');");
+	        out.println("location='UserView.jsp';");
 	        out.println("</script>");
+	        System.out.println("hp inval");
+	        valid = false;
+	        
 		}
-		else if (pwd.equals(conPwd)==false) {
+		if (valid) {
+			UserViewDao ud=new UserViewDao();
+			ud.updateDetails(email, pAddress, cAddress, mobNo, homeNo,select);
 			out.println("<script type=\"text/javascript\">");
-	        out.println("alert('Your password mismatch.');");
-	        out.println("location='Register.jsp';");
-	        out.println("</script>");
-		}
-		else if (mobNo.contains("[0-9]+") && mobNo.length() != 10) {;
-			out.println("<script type=\"text/javascript\">");
-	        out.println("alert('Invalid Mobile No.\nCannot contain characters');");
-	        out.println("location='Register.jsp';");
-	        out.println("</script>");
-		}
-		else if (homeNo.contains("[0-9]+") && homeNo.length() != 10) {
-			out.println("<script type=\"text/javascript\">");
-	        out.println("alert('Invalid Home No.\nCannot contain characters');");
-	        out.println("location='Register.jsp';");
-	        out.println("</script>");
-		}
-		else {
-//			RegisterDao Rd=new RegisterDao();
-//			Rd.registerDetails(fname,lname, sqlStartDate, pAddress, cAddress, mobNo, homeNo, email,perform.encrypt(pwd),select);
-			out.println("<script type=\"text/javascript\">");
-	        out.println("alert('Register Successful !!');");
-	        out.println("location='index.jsp';");
+	        out.println("alert('Update Successful !!');");
+	        out.println("location='UserView.jsp';");
 	        out.println("</script>");
 		}
 	}
